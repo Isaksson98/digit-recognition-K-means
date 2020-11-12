90b0bd359b591403d02dedc84bb816deef6e7ffd
filip <filis220@student.liu.se>
2020-11-12 13:23:05 +0100

    calculated cross entropy

--- digit-recognition.R
+++ digit-recognition.R
@@ -24,6 +24,7 @@ train_data=train[, 1:64]
 #train_labels=train[, 65]
 train_labels=as.factor(train$V65)
 test_labels=as.factor(test$V65)
+valid_labels=as.factor(valid$V65)
 
 kknn_model_train = kknn(formula=train_labels~., test=test, train=train, k=30, kernel="rectangular")
 kknn_model_test = kknn(formula=test_labels~., test=test, train=test, k=30, kernel="rectangular")
@@ -46,26 +47,29 @@ missclass=function(X,X1){
 }
 
 missclass(fit_train, test_labels)
-missclass_rate_train = 1-sum(diag(conf_matrix_train)/sum(conf_matrix_train))
-missclass_rate_test = 1-sum(diag(conf_matrix_test)/sum(conf_matrix_test))
 missclass_rate_train
 num4 = mean(fit_train != test_labels)
 num4
 ###STEP 3###
 
-list_of_8 = kknn_model_train$prob
-list_of_8
-num = sort(list_of_8, decreasing = FALSE)
 
-num1 = 14
-imge=train_data[num1,]
+probs = kknn_model_train$prob
+
+#Row 7, 8 & 15 is low
+#Row 49, 50 is high
+num1 = 50
+imge=test_data[num1,]
 d <- matrix(imge, nrow = 8, byrow = TRUE)
 y = matrix(as.numeric(d), nrow = 8)
-
+y=t(y)
 heatmap(y, Colv=NA, Rowv=NA)
-imge2=train[num1,]
+imge2=test[num1,]
 imge2$V65
 
+#7 is 3
+#8 is 9
+#15 is 3
+#49&50 is 8
 ###STEP 4###
 m <- vector("integer", 30)
 s <- vector("integer", 30)
@@ -75,11 +79,10 @@ for(k in 1:30){
   fit_train_loop_m <- fitted(kknn_model_loop_m)
   m[k] <- missclass(fit_train_loop_m, test_labels)
 
-  kknn_model_loop_s = kknn(formula=test_labels~., test=test, train=test, k=k, kernel="rectangular")
+  kknn_model_loop_s = kknn(formula=train_labels~., test=valid, train=train, k=k, kernel="rectangular")
   fit_train_loop_s <- fitted(kknn_model_loop_s)
-  s[k] <- missclass(fit_train_loop_s, test_labels)
+  s[k] <- missclass(fit_train_loop_s, valid_labels)
 }
-
 plot( m, type='l', col='orange', main='Errors on test data',
      xlab='K', ylab='Missclass_err')
 lines( s, col='blue')
@@ -87,9 +90,25 @@ lines( s, col='blue')
 
 ###STEP 5###
 
+kknn_model_loop_m$prob
 
+epsilon=1.0e-15
 
+for(k in 1:30){
+  kknn_model_loop_m2 = kknn(formula=train_labels~., test=test, train=train, k=k, kernel="rectangular")
+  fit_train_loop_m2 <- fitted(kknn_model_loop_m2)
 
+  m_c = missclass(fit_train_loop_m2, test_labels)
+  p = 1-m_c
+  H[k] = -p*log(p+epsilon)
+  
+}
+H
+
+plot( H, type='l', col='green', main='Cross entropy',
+      xlab='K', ylab='Missclass_err')
+#Cross entropy is more suitable because it factors in the 
+# probability for each guess. say 90% sure instead of 70% is better
 
 
 
